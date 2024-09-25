986078275db7d45a562871036376e1a6cb1a9deb
GUSTAVO MUZEKA <gustavo@suzuki.dominio>
2024-09-25 14:56:13 -0300

    Finalizada adição dos projetos.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,15 @@
 const glowDiv = document.querySelector('.glow');
 const donwloadCV = document.querySelector('.donwload')
+const lamp = document.querySelector('.lamp')
+const texto = document.querySelector('.texto')
+
+texto.addEventListener('mouseenter', (animei) => {
+    lamp.classList.remove('hidden');
+})
+
+texto.addEventListener('mouseout', (animei) => {
+    lamp.classList.add('hidden');
+})
 
 document.addEventListener('mousemove', (e) => {
     const x = e.clientX;
@@ -7,6 +17,8 @@ document.addEventListener('mousemove', (e) => {
     
     glowDiv.style.background = `radial-gradient(600px at ${x}px ${y}px, rgba(29, 78, 216, 0.3), transparent 80%)`;
 });
+
+
 
 donwloadCV.addEventListener('mouseenter',(entrou) =>{
     donwloadCV.classList.remove('animacao')
